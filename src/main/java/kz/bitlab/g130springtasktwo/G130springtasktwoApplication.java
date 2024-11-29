a59948da6c2c600ee701d4d3ff61b694594317fb
kuatabylay <abylaykuat@gmail.com>
2024-11-29 19:47:33 +0500

    initial commit

---
+++ src/main/java/kz/bitlab/g130springtasktwo/G130springtasktwoApplication.java
@@ -0,0 +1,13 @@
+package kz.bitlab.g130springtasktwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G130springtasktwoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(G130springtasktwoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kz/bitlab/g130springtasktwo/G130springtasktwoApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.bitlab.g130springtasktwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G130springtasktwoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
